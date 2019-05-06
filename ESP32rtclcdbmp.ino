a5e579cf2d3b95e8cabfd3c2ce71d9b19855ac00
Restu Adi Pradana <restu.maduki@live.com>
2019-05-06 09:19:59 +0700

    Create ESP32rtclcdbmp.ino

---
+++ ESP32rtclcdbmp.ino
@@ -0,0 +1,214 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x64 size display using SPI to communicate
+4 or 5 pins are required to interface
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SFE_BMP180.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+// If using software SPI (the default case):
+#define OLED_MOSI   23
+#define OLED_CLK   18
+#define OLED_DC    4
+#define OLED_CS    5
+#define OLED_RESET 15
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+/* Uncomment this block to use hardware SPI
+#define OLED_DC     6
+#define OLED_CS     7
+#define OLED_RESET  8
+Adafruit_SSD1306 display(OLED_DC, OLED_RESET, OLED_CS);
+*/
+SFE_BMP180 pressure;
+#define ALTITUDE 25.0
+RTC_DS3231 rtc;
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup()   {                
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC);
+  // init done
+  
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(2000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+
+  // draw a single pixel
+  display.drawPixel(10, 10, WHITE);
+  // Show the display buffer on the hardware.
+  // NOTE: You _must_ call display after making any drawing commands
+  // to make them visible on the display hardware!
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  
+
+  if (pressure.begin()){
+    display.println("BMP180 init success");
+    display.display();
+    delay(1000);}
+  else
+  {
+    // Oops, something went wrong, this is usually a connection problem,
+    // see the comments at the top of this sketch for the proper connections.
+
+    display.println("BMP180 init fail\n\n");
+    display.display();
+    delay(1000);
+    while(1); // Pause forever.
+  }
+  
+  display.clearDisplay();
+  // draw many lines
+
+
+  // text display tests
+
+}
+
+
+void loop() {
+  DateTime now = rtc.now();
+  char status;
+  double T,P,p0,a;
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+
+   status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      // Print out the measurement:
+      display.print("Suhu: ");
+      display.print(T,2);
+      display.println(" C ");
+      
+      
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          display.print("Tekanan: ");
+          display.print(P,2);
+          display.print(" mb, ");
+          display.print(P*0.0295333727,2);
+          display.println(" inHg");
+
+          // The pressure sensor returns abolute pressure, which varies with altitude.
+          // To remove the effects of altitude, use the sealevel function and your current altitude.
+          // This number is commonly used in weather reports.
+          // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+          // Result: p0 = sea-level compensated pressure in mb
+
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          display.print("Tek DPL: ");
+          display.print(p0,2);
+          display.print(" mb, ");
+          display.print(p0*0.0295333727,2);
+          display.println(" inHg");
+
+          // On the other hand, if you want to determine your altitude from the pressure reading,
+          // use the altitude function along with a baseline pressure (sea-level or other).
+          // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
+          // Result: a = altitude in m.
+
+          a = pressure.altitude(P,p0);
+          display.print("Ketinggian: ");
+          display.print(a,0);
+          display.println(" m ");
+        }
+        else display.println("error retrieving pressure measurement\n");
+      }
+      else display.println("error starting pressure measurement\n");
+    }
+    else display.println("error retrieving temperature measurement\n");
+  }
+  else display.println("error starting temperature measurement\n");
+
+
+
+display.setTextSize(2);
+display.print(now.hour(), DEC);
+display.print(':');
+display.print(now.minute(), DEC);
+display.print(':');
+display.println(now.second(), DEC);
+
+
+
+display.display();
+  delay(1000);  // Pause for 5 seconds.
+
+display.clearDisplay();
+
+
+ 
+
+}
+
+
+
+
